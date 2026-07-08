41d28d35b640e9f316871b09f482036b8d95376d
KitsuneAlex <alexh@karmakrafts.dev>
2026-07-08 02:34:46 +0200

    Update 4x4 float matrix JOML benchmark on JVM to mirror cases of KGML benchmark

--- kgml-benchmarks/src/jvmMain/kotlin/dev/karmakrafts/kgml/JomlMatrix4x4fArithmeticBenchmark.kt
+++ kgml-benchmarks/src/jvmMain/kotlin/dev/karmakrafts/kgml/JomlMatrix4x4fArithmeticBenchmark.kt
@@ -23,12 +23,121 @@ import org.joml.Matrix4f
 
 @State(Scope.Benchmark)
 open class JomlMatrix4x4fArithmeticBenchmark {
-    val m1 = Matrix4f().perspective(70F, 1920F / 1080F, 0.1F, 100F)
-    val m2 = Matrix4f().rotation(Math.toRadians(45.0).toFloat(), 1F, 0F, 0F)
+    val genericLeft = Matrix4f().set(
+        1.25F,
+        2.5F,
+        3.75F,
+        4.25F,
+        5.5F,
+        6.75F,
+        7.25F,
+        8.5F,
+        9.75F,
+        10.25F,
+        11.5F,
+        12.75F,
+        13.25F,
+        14.5F,
+        15.75F,
+        16.25F
+    )
+    val genericRight = Matrix4f().set(
+        16.25F,
+        15.75F,
+        14.5F,
+        13.25F,
+        12.75F,
+        11.5F,
+        10.25F,
+        9.75F,
+        8.5F,
+        7.25F,
+        6.75F,
+        5.5F,
+        4.25F,
+        3.75F,
+        2.5F,
+        1.25F
+    )
+    val genericScale = Matrix4f().set(
+        4F, 0F, 0F, 0F, 0F, 5F, 0F, 0F, 0F, 0F, 6F, 0F, 0F, 0F, 0F, 1F
+    )
+    val genericScaleRight = Matrix4f().set(
+        7F, 0F, 0F, 0F, 0F, 8F, 0F, 0F, 0F, 0F, 9F, 0F, 0F, 0F, 0F, 1F
+    )
+    val translation = Matrix4f().translation(2F, 3F, 4F)
+    val translationRight = Matrix4f().translation(5F, 7F, 11F)
+    val scale = Matrix4f().scaling(4F, 5F, 6F)
+    val scaleRight = Matrix4f().scaling(7F, 8F, 9F)
+    val rotation = Matrix4f().rotationZ(Math.toRadians(45.0).toFloat())
+    val rotationRight = Matrix4f().rotationZ(Math.toRadians(30.0).toFloat())
+    val genericRotation = Matrix4f(rotation)
+    val genericRotationRight = Matrix4f(rotationRight)
+    val perspective = Matrix4f().perspective(70F, 1920F / 1080F, 0.1F, 100F)
 
-    @JvmName("run")
+    @JvmName("generic")
     @Benchmark
-    fun run(): Matrix4f {
-        return m1.mul(m2, Matrix4f())
+    fun generic(): Matrix4f {
+        return genericLeft.mul(genericRight, Matrix4f())
+    }
+
+    @JvmName("genericDiagonalDiagonal")
+    @Benchmark
+    fun genericDiagonalDiagonal(): Matrix4f {
+        return genericScale.mul(genericScaleRight, Matrix4f())
+    }
+
+    @JvmName("genericRotationRotation")
+    @Benchmark
+    fun genericRotationRotation(): Matrix4f {
+        return genericRotation.mul(genericRotationRight, Matrix4f())
+    }
+
+    @JvmName("linearDiagonal")
+    @Benchmark
+    fun linearDiagonal(): Matrix4f {
+        return rotation.mul(scale, Matrix4f())
+    }
+
+    @JvmName("diagonalLinear")
+    @Benchmark
+    fun diagonalLinear(): Matrix4f {
+        return scale.mul(rotation, Matrix4f())
+    }
+
+    @JvmName("diagonalDiagonal")
+    @Benchmark
+    fun diagonalDiagonal(): Matrix4f {
+        return scale.mul(scaleRight, Matrix4f())
+    }
+
+    @JvmName("rotationRotation")
+    @Benchmark
+    fun rotationRotation(): Matrix4f {
+        return rotation.mul(rotationRight, Matrix4f())
+    }
+
+    @JvmName("affineLinear")
+    @Benchmark
+    fun affineLinear(): Matrix4f {
+        return translation.mul(scale, Matrix4f())
+    }
+
+    @JvmName("linearTranslation")
+    @Benchmark
+    fun linearTranslation(): Matrix4f {
+        return scale.mul(translation, Matrix4f())
+    }
+
+    @JvmName("translationTranslation")
+    @Benchmark
+    fun translationTranslation(): Matrix4f {
+        return translation.mul(translationRight, Matrix4f())
+    }
+
+    @JvmName("perspectiveLinear")
+    @Benchmark
+    fun perspectiveLinear(): Matrix4f {
+        return perspective.mul(rotation, Matrix4f())
     }
 }
